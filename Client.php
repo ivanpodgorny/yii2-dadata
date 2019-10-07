ab52fa699b122a6c1cfec231210a54d7b3d18d89
i.podgornyy <i.podgorny@digital-sector.ru>
2019-10-07 12:46:55 +0300

    Add suggest address

--- Client.php
+++ Client.php
@@ -2,6 +2,7 @@
 
 namespace gietos\yii\Dadata;
 
+use Dadata\Response\Address;
 use yii\base\Configurable;
 
 /**
@@ -9,4 +10,26 @@ use yii\base\Configurable;
  */
 class Client extends \Dadata\Client implements Configurable
 {
+    /**
+     * @param string $address
+     * @param int $count
+     * @return Address[]
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \ReflectionException
+     */
+    public function suggestAddress(string $address, int $count = 10): array
+    {
+        $result = $this->query($this->prepareSuggestionsUri('suggest/address'), [
+            'query' => $address,
+            'count' => $count,
+        ]);
+
+        $addresses = [];
+        foreach ($result as $searchData) {
+            $addresses[] = $this->populate(new Address, $searchData['data']);
+        }
+
+        return $addresses;
+    }
+
 }
